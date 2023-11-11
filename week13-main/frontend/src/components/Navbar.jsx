80838143efc885dd6e3ba72dca8ef0fe09110b99
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2023-11-11 19:44:32 +0700

    Add files via upload

---
+++ week13-main/frontend/src/components/Navbar.jsx
@@ -0,0 +1,152 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { loginUser } from "../modules/fetch";
+
+const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isLogin, setIsLogin] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      setIsLogin(true);
+    }
+  }, [window.localStorage.getItem("token")]);
+
+  return (
+    <Flex
+      w="full"
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1rem"
+      bg="gray.800"
+      color="white"
+      margin='0.5%'
+      width="95%"
+      borderRadius="xl"
+      position='sticky'
+      zIndex= 'sticky'
+      top= {0}
+    >
+      <Link to="/">
+        <Flex align="center" mr={5} cursor="pointer">
+          <Text fontSize="xl" fontWeight="bold">
+          BintangBooks
+          </Text>
+        </Flex>
+      </Link>
+      <HStack>
+        {isLogin && (
+          <Link to="/newbook">
+            <Button colorScheme="blue">Create New Book</Button>
+          </Link>
+        )}
+        {!isLogin ? (
+          <Button onClick={onOpen} colorScheme="blue">
+            Login
+          </Button>
+        ) : (
+          <Button
+            colorScheme="red"
+            onClick={() => {
+              window.localStorage.removeItem("token");
+              setIsLogin(false);
+              navigate("/")
+            }}
+          >
+            Logout
+          </Button>
+        )}
+      </HStack>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <form
+          id="login-form"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            try {
+              const token = await loginUser(
+                e.target.email.value,
+                e.target.password.value
+              );
+              window.localStorage.setItem("token", token.token);
+              navigate("/");
+              onClose();
+            } catch (err) {
+              toast({
+                title: "Error",
+                description: err.message,
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+              });
+            }
+          }}
+        >
+          <ModalOverlay />
+          <ModalContent bgColor='gray.700'>
+            <ModalHeader color='white'>Login</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack>
+                <FormControl isRequired>
+                  <FormLabel color='white'>Email</FormLabel>
+                  <Input
+                    name="email"
+                    type="email"
+                    placeholder="Enter your email address"
+                    bgColor = 'white'
+                  />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel color='white'>Password</FormLabel>
+                  <Input
+                    type="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    bgColor = 'white'
+                  />
+                </FormControl>
+              </VStack>
+            </ModalBody>
+            <ModalFooter>
+              <Button type="submit" form="login-form" colorScheme="blue" mr={3}>
+                Login
+              </Button>
+              <Link to="/register" onClick={onClose}>
+                <Button  color='white' bgColor='green' _hover={{ bgColor: 'darkgreen' }}>
+                  Doesn't Have Account? Click here
+                </Button>
+              </Link>
+            </ModalFooter>
+          </ModalContent>
+        </form>
+      </Modal>
+    </Flex>
+  );
+};
+
+export default Navbar;
